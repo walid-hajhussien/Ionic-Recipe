fbc56ac424e2d63d9ecd0102f364287629628347
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-19 12:30:11 +0300

    add recipe list

--- src/app/components/recipe/recipe.page.ts
+++ src/app/components/recipe/recipe.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RecipeInterface} from '../../interfaces/recipe.interface';
 
 @Component({
-  selector: 'app-recipe',
-  templateUrl: './recipe.page.html',
-  styleUrls: ['./recipe.page.scss'],
+    selector: 'app-recipe',
+    templateUrl: './recipe.page.html',
+    styleUrls: ['./recipe.page.scss'],
 })
 export class RecipePage implements OnInit {
+    public recipes: RecipeInterface[] = [{
+        id: '1',
+        title: 'Chicken',
+        imageUrl: 'https://cafedelites.com/wp-content/uploads/2018/06/Garlic-Mushroom-Chicken-Thighs-IMAGE-1.jpg',
+        ingredients: ['chicken,Bread,milk']
+    },{
+      id: '2',
+      title: 'Meat',
+      imageUrl: 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/roast-beef-recipes.jpg',
+      ingredients: ['Meat,Bread,milk']
+    }]
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
---
+++ src/app/interfaces/recipe.interface.ts
@@ -0,0 +1,6 @@
+export interface RecipeInterface {
+    id: string;
+    title: string;
+    imageUrl: string;
+    ingredients: string[];
+}
