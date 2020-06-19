e861915699e8d9309cba473638d2702ff02624bd
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-19 18:31:19 +0300

    (add) ionic alert

--- src/app/components/recipe-details/recipe-details.page.ts
+++ src/app/components/recipe-details/recipe-details.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {RecipeInterface} from '../../interfaces/recipe.interface';
 import {RecipeService} from '../../services/recipe/recipe.service';
+import {AlertController} from '@ionic/angular';
 
 @Component({
     selector: 'app-recipe-details',
@@ -12,7 +13,12 @@ export class RecipeDetailsPage implements OnInit {
     private id: string;
     public recipe: RecipeInterface;
 
-    constructor(private activatedRoute: ActivatedRoute, private recipeService: RecipeService, private router: Router) {
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private recipeService: RecipeService,
+        private router: Router,
+        private alertController: AlertController) {
+
         this.activatedRoute.paramMap.subscribe((params) => {
             if (!params.has('id')) {
                 // todo: redirect to recipe
@@ -29,6 +35,40 @@ export class RecipeDetailsPage implements OnInit {
     onDelete() {
         this.recipeService.deleteRecipe(this.id);
         this.router.navigate(['../../'], {relativeTo: this.activatedRoute});
+    }
+
+    async onConfirmDelete() {
+        const alert = await this.alertController.create({
+            header: 'Delete',
+            message: 'Are you sure you want to Delete' + `<p>${this.recipe.title}</p>`,
+            buttons: ['Cancel', {
+                text: 'Okay',
+                handler: () => {
+                    this.onDelete();
+                }
+            }]
+        });
+
+        await alert.present();
+
+    }
+
+    onConfirmDelete2() {
+        const alert = this.alertController.create({
+            header: 'Delete',
+            message: 'Are you sure you want to Delete' + `<p>${this.recipe.title}</p>`,
+            buttons: ['Cancel', {
+                text: 'Okay',
+                handler: () => {
+                    this.onDelete();
+                }
+            }]
+        });
+
+        alert.then((alertEl: HTMLIonAlertElement) => {
+            alertEl.present().then(() => {
+            });
+        });
     }
 
 }
