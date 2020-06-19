2f510ca5006ea764bc1e01c6c5f02604869b9f0b
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-19 16:53:29 +0300

    (add) recipe detailes logic

--- src/app/components/recipe-details/recipe-details.page.ts
+++ src/app/components/recipe-details/recipe-details.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {RecipeInterface} from '../../interfaces/recipe.interface';
+import {RecipeService} from '../../services/recipe/recipe.service';
 
 @Component({
-  selector: 'app-recipe-details',
-  templateUrl: './recipe-details.page.html',
-  styleUrls: ['./recipe-details.page.scss'],
+    selector: 'app-recipe-details',
+    templateUrl: './recipe-details.page.html',
+    styleUrls: ['./recipe-details.page.scss'],
 })
 export class RecipeDetailsPage implements OnInit {
+    private id: string;
+    public recipe: RecipeInterface;
 
-  constructor() { }
+    constructor(private activatedRoute: ActivatedRoute, private recipeService: RecipeService) {
+        this.activatedRoute.paramMap.subscribe((params) => {
+            if (!params.has('id')) {
+                // todo: redirect to recipe
+                return;
+            }
+            this.id = params.get('id');
+            this.recipe = recipeService.getRecipeById(this.id);
+        });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/services/recipe/recipe.service.ts
+++ src/app/services/recipe/recipe.service.ts
@@ -9,12 +9,12 @@ export class RecipeService {
         id: '1',
         title: 'Chicken',
         imageUrl: 'https://cafedelites.com/wp-content/uploads/2018/06/Garlic-Mushroom-Chicken-Thighs-IMAGE-1.jpg',
-        ingredients: ['chicken,Bread,milk']
+        ingredients: ['chicken', 'Bread', 'milk']
     }, {
         id: '2',
         title: 'Meat',
         imageUrl: 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/roast-beef-recipes.jpg',
-        ingredients: ['Meat,Bread,milk']
+        ingredients: ['Meat', 'Bread', 'milk']
     }];
 
     constructor() {
