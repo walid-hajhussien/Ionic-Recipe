b0d3f50268e50c4d974162a60b72cdb970d2b337
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-19 12:51:33 +0300

    add recipe details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,33 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'recipe',
-    pathMatch: 'full'
-  },
-  {
-    path: 'recipe',
-    loadChildren: () => import('./components/recipe/recipe.module').then( m => m.RecipePageModule)
-  },
+    {
+        path: '',
+        redirectTo: 'recipe',
+        pathMatch: 'full'
+    },
+    {
+        path: 'recipe',
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('./components/recipe/recipe.module').then(m => m.RecipePageModule)
+            },
+            {
+                path: ':id',
+                loadChildren: () => import('./components/recipe-details/recipe-details.module').then(m => m.RecipeDetailsPageModule)
+            },
+        ]
+    },
+
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/components/recipe/recipe-routing.module.ts
+++ src/app/components/recipe/recipe-routing.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { RecipePage } from './recipe.page';
+import {RecipePage} from './recipe.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: RecipePage
-  }
+    {
+        path: '',
+        component: RecipePage
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class RecipePageRoutingModule {}
+export class RecipePageRoutingModule {
+}
