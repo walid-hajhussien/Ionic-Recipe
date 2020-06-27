2bf0ae8ff495f753a7e07d08f697f9529930d369
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-27 08:20:58 +0300

    (add) ionic lifecycle

--- src/app/pages/recipe/recipe.page.ts
+++ src/app/pages/recipe/recipe.page.ts
@@ -10,17 +10,37 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class RecipePage implements OnInit {
     recipes: RecipeInterface[] = [];
+    counter: number;
 
     constructor(private recipeService: RecipeService, private router: Router, private activatedRoute: ActivatedRoute) {
+        this.counter = 0;
     }
 
     ngOnInit() {
         this.recipes = this.recipeService.recipes;
+        console.log('recipes', this.recipes);
     }
 
 
     onShowRecipe(id: string): void {
         this.router.navigate([id], {relativeTo: this.activatedRoute});
+    }
+
+
+    ionViewWillEnter() {
+        console.log('ionViewWillEnter', ++this.counter);
+    }
+
+    ionViewDidEnter() {
+        console.log('ionViewDidEnter', ++this.counter);
+    }
+
+    ionViewWillLeave() {
+        console.log('ionViewWillLeave', ++this.counter);
+    }
+
+    ionViewDidLeave() {
+        console.log('ionViewDidLeave', ++this.counter);
     }
 
 }
